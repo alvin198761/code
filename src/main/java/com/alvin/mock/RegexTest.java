32be246f253a9f43980a900f79953bf51bbbad56
tangzhichao <alvin198761@163.com>
2018-09-21 19:12:16 +0800

    改模板，增加自定义返回结果，和自定义查询

--- src/main/java/com/alvin/mock/RegexTest.java
+++ src/main/java/com/alvin/mock/RegexTest.java
@@ -1,16 +1,21 @@
 package com.alvin.mock;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class RegexTest {
 
 	public static void main(String  [] args) {
-		String regex ="(\\d+)";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher m = pattern.matcher("[[72093367,72117435,72151455,1]]");
-		while(m.find()){
-			System.out.println(m.group(1));
-		}
+//		String regex ="(\\d+)";
+//		Pattern pattern = Pattern.compile(regex);
+//		Matcher m = pattern.matcher("[[72093367,72117435,72151455,1]]");
+//		while(m.find()){
+//			System.out.println(m.group(1));
+//		}
+//		String text ="the  data the data 234";
+//		Pattern pattern = Pattern.compile("([a-zA-Z]+)\\s+");
+//		Matcher m = pattern.matcher(text);
+//		while(m.find()){
+//			text = text.replaceFirst(m.group(1)+"\\s" ,m.group(1) +"1 ");
+//		}
+//		System.out.println(text);
+
 	}
 }
