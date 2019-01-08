035c0a791d4d7fa8aae5b5536674d62a2726edd5
tangzhichao <alvin198761@163.com>
2019-01-08 18:02:30 +0800

    增加统一返回结果的模板，尚未测试

--- src/main/java/com/gzz/createcode/Number.java
+++ src/main/java/com/gzz/createcode/Number.java
@@ -1,9 +1,24 @@
 package com.gzz.createcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Number {
 
 	public static void main(String[] args) {
+		List<String> list1 = new ArrayList<String>();
+		list1.add("A");
+		list1.add("B");
+		list1.add("C");
+		list1.add("D");
+		List<String> list2 = new ArrayList<String>();
+		list2.add("B");
+		list2.add("C");
+		list2.add("F");
 
+//		list1.retainAll(list2);
+		list2.retainAll(list1);
+		System.out.println(list2);//运行结果
 
 	}
 }
